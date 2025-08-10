b2230d519f2035e7bcc29f452ee0545eeb6d4cfe
Samanta Mesquita <samanta464.barros@gmail.com>
2025-08-10 20:24:08 -0300

    feat: add service, configs and organize structure

---
+++ src/types/operations/operation.ts
@@ -0,0 +1,7 @@
+export interface Operation {
+  id: string;
+  description: string;
+  type: "Embarque" | "Desembarque";
+  terminal: "Terminal Sul" | "Terminal Norte" | "Terminal Oeste" | "Terminal Leste";
+  status: "Criada" | "Processando" | "Finalizada";
+}
