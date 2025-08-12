d3f34c4aa9cf229a33575ded7325b77f2125ca4a
Samanta Mesquita <samanta464.barros@gmail.com>
2025-08-11 23:51:16 -0300

    feat: add header to main

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import './index.css'
 import App from './App.tsx'
 import { Theme } from '@radix-ui/themes'
 import { ToastProvider } from './components/toast/ToastProvider.tsx'
+import { Header } from './components/header';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Theme>
       <ToastProvider>
+          <Header />  
         <App />
       </ToastProvider>
     </Theme>
