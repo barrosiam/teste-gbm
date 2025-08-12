a89f1be497833cf8741fa8891f76f57b526b6d3c
Samanta Mesquita <samanta464.barros@gmail.com>
2025-08-12 00:08:57 -0300

    feat: add svg and header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,28 @@
+export function Header() {
+  return (
+    <header className="sticky top-0 z-40 w-full bg-white/90 backdrop-blur border-b border-black/5">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="h-14 flex items-center justify-between">
+          <a href="/" className="inline-flex items-center gap-2">
+            <img
+              src="../../svg/gbm-logo.svg" //add logo porque ta quebrada
+              alt="Logo da GBM"
+              className="h-8 w-auto w-[106px] 2xl:w-[124px]"
+            />
+          </a>
+
+          <nav>
+            <a
+              href="https://www.gbmtech.com.br/pt-br"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-medium transition-colors"
+            >
+              Sobre
+            </a>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
