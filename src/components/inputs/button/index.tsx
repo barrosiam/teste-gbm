1c11981fa2b4dc3e05bce355b3093ac8cec95036
Samanta Mesquita <samanta464.barros@gmail.com>
2025-08-11 23:42:59 -0300

    feat: improve button to mobile version

--- src/components/inputs/button/index.tsx
+++ src/components/inputs/button/index.tsx
@@ -4,6 +4,7 @@ type Props = {
   label: string
   onClick?: () => void
   type?: 'reset' | 'submit' | 'button'
+  classInfo?: string
 }
 
 export const Button = ({
@@ -12,11 +13,12 @@ export const Button = ({
   label,
   onClick,
   type = 'button',
+  classInfo,
 }: Props) => {
   return (
-    <div>
+    <div className={`${classInfo}`} >
       <button
-        className={`rounded-md transition-colors bg-${color} rounded p-3 font-medium text-white hover:bg-${color} disabled:opacity-50`}
+        className={`rounded-md ${classInfo} transition-colors bg-${color} rounded p-3 font-medium text-white hover:bg-${color} disabled:opacity-50`}
         onClick={onClick}
         disabled={disabled}
         type={type}
